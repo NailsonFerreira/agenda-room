551f97b37ccae6e1894502c21a2dcb665d14a95f
Nailson <nailson_ferreira@hotmail.com>
2019-05-15 10:54:40 -0300

    Add Jetpack, doing CRUD

--- app/src/main/java/br/com/alura/agenda/database/AgendaDatabase.java
+++ app/src/main/java/br/com/alura/agenda/database/AgendaDatabase.java
@@ -1,7 +1,23 @@
 package br.com.alura.agenda.database;
 
 import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
 
-@Database(entities = {}, version = 1)
-public class AgendaDatabase {
+import br.com.alura.agenda.database.dao.AlunoDAO;
+import br.com.alura.agenda.model.Aluno;
+
+@Database(entities = {Aluno.class}, version = 1, exportSchema = false)
+public abstract class AgendaDatabase extends RoomDatabase{
+
+    private static final String AGENDA_DB = "agenda.db";
+
+    public abstract AlunoDAO getRoomAlunoDAO();
+
+    public static AgendaDatabase getInstance(Context context){
+        return Room.databaseBuilder(context, AgendaDatabase.class, AGENDA_DB)
+                .allowMainThreadQueries()
+                .build();
+    }
 }
